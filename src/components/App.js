8759976588eb6d934eea9d80cbe7e227ce31bd4e
Simon Gussenhoven <simongussenhoven@gmail.com>
2020-12-16 21:58:11 +0100

    Rendering a list item

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import Nav from './Nav'
+import List from './List'
+
+function App() {
+  return (
+    <div className="App">
+      <Nav/>
+      <List/>
+    </div>
+  );
+}
+
+export default App;
