d37d2eb06e185ebc26e9fd4f4ad7df5a0f1c578a
Simon Gussenhoven <simongussenhoven@gmail.com>
2020-12-16 23:09:37 +0100

    Delete function added

--- src/components/List.js
+++ src/components/List.js
@@ -2,7 +2,7 @@ import ListItem from './ListItem'
 
 function List(props) {
     const items = props.songs.map(song => {
-      return <ListItem {...song}/>
+      return <ListItem {...song} deleteItem={props.deleteItem}/>
     })
     return (
     <div className="list">
